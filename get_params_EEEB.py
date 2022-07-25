50df25d867943bbfd6ef343dc117c4657565bfcb
Davide Valsecchi <davide.valsecchi@cern.ch>
2022-07-25 13:46:53 +0200

    fix

--- get_params_EEEB.py
+++ get_params_EEEB.py
@@ -4,7 +4,7 @@ EB = pd.read_csv("ChannelNumberingEB.csv", sep=',')
 EE = pd.read_csv("ChannelNumberingEE.csv", sep=',')
 
 EBparams = EB[EB["Xtal in VFE"]==1][["stripid","CMSSWID", "FED", "TT", "VFE"]]
-EEparams = EE[EE["Xtal in VFE"==1]][["stripid", "CMSSWID", "FED", "CCU", "VFE"]]
+EEparams = EE[EE["Xtal in VFE"]==1][["stripid", "CMSSWID", "FED", "CCU", "VFE"]]
 
 
 EBparams.to_csv("params_EB.csv", index=False, sep=",")
